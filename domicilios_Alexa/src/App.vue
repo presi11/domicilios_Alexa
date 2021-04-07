b61960b4e165ad4bab4469b2ca1c42f09205dc25
presi11 <juan.ospina19@udea.edu.co>
2021-04-07 16:36:42 -0500

    first

---
+++ domicilios_Alexa/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ helloWorld }}</div>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  apollo: {
+    // Simple query that will update the 'hello' vue property
+    helloWorld: gql`query holamundo{
+      helloWorld
+    }`,
+  },
+}
+</script>
