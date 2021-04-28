d51c28b896ccdfb35f3308fa12793bc86b385a91
presi11 <juan.ospina19@udea.edu.co>
2021-04-28 15:11:24 -0500

    Accestoken

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-            <!-- NEW - add a route to the profile page -->
-      <router-link v-if="$auth.isAuthenticated" to="/profile">Profile</router-link>
+      <router-link to="/about">About</router-link>|
+      <router-link v-if="$auth.isAuthenticated" to="/profile">Profile</router-link>|
+      <router-link to="/external-api">External Api</router-link>
     </div>
     <router-view/>
   </div>
